b7f45a6a368609210c1e56ade718efda30670e34
yuramayer <votetopovorot2015@gmail.com>
2024-12-10 14:09:18 +0300

    added .env solution to the config

--- config/values.py
+++ config/values.py
@@ -1,5 +1,13 @@
 import os
 
+
+env_file = '.env'
+
+if os.path.isfile(env_file):
+    from dotenv import load_dotenv
+    load_dotenv()
+
+
 BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
 OPEN_AI_API_KEY = str(os.getenv("OPEN_AI_API_KEY"))
 DB_URI = str(os.getenv("DB_URI"))
